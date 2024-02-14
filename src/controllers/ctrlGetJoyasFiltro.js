20e8c753f71fcc41c395328692270ea0eea03974
HendyCastro <hendy0702@gmail.com>
2024-02-14 02:56:01 -0300

    agregando punto 2 de los requerimientos

--- src/controllers/ctrlGetJoyasFiltro.js
+++ src/controllers/ctrlGetJoyasFiltro.js
@@ -2,36 +2,42 @@ const db = require('../database/dbindex');
 const { selectFiltros } = require('../database/querys/queryindex');
 
 const getJoyasFiltro = async (req, res) => {
-const filtro = req.query; 
-let query = '';
-switch(filtro){
-    case filtro === undefined:
-        return res.status(400).json({
-            msg: 'invalid or missing filter'
-        });
-    case filtro === 'precio_max':
-        query = 'precio DESC'
-        break;
-    case filtro === 'precio_min':
-        query = 'precio ASC'
-        break;
-    case filtro === 'categoria' || filtro === 'metal':
-        query = filtro
-        break;
-    default:
-        res.status(400).json({
-            msg: 'Please provide a valid filter'
-        });
-        break;
+const { precio_min, precio_max, categoria, metal} = req.query; 
+const queryParams = [];
+
+let query = selectFiltros;
+
+
+if (precio_min){
+    queryParams.push(precio_min);
+    query += ' AND precio >= $' + queryParams.length;
+    }
+
+if (precio_max){
+    queryParams.push(precio_max);
+    query += ' AND precio <= $' + queryParams.length;
 }
+
+if (categoria){
+    queryParams.push(categoria);
+    query += ' AND categoria = $' + queryParams.length;
+}
+
+if (metal){
+    queryParams.push(metal);
+    query += ' AND metal = $' + queryParams.length;
+}
+
 try {
-    const { rowCount, rows } = await db.query(selectFiltros, query);
+    const { rowCount, rows } = await db.query(query, queryParams);
+    
     if (rowCount > 0) {
         res.status(200).json({
             msg: 'Data fetch successfuly',
             dataCount: rowCount,
             data: rows,
         });
+
     } else {
         res.status(200).json({
             msg: 'No data found',
