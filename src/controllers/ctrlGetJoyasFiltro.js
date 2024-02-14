c4838cd90c4fb931899674b7f6f7c62d7c791b39
HendyCastro <hendy0702@gmail.com>
2024-02-14 03:22:40 -0300

    Agregado código y validaciones para el endpoint de filtrado de joyas

--- src/controllers/ctrlGetJoyasFiltro.js
+++ src/controllers/ctrlGetJoyasFiltro.js
@@ -8,11 +8,27 @@ const queryParams = [];
 let query = selectFiltros;
 
 
+if (!precio_min || !precio_max || !categoria || !metal){
+    return res.status(400).json({ msg: 'Faltan variables requeridas' });
+}
+
+if (isNaN(precio_min) || isNaN(precio_max) || parseFloat(precio_min) < 0 || parseFloat(precio_max) < 0) {
+    return res.status(400).json({ msg: 'Los precios deben ser numeros positivos' });
+}
+
+if (parseFloat(precio_min) > parseFloat(precio_max)) {
+    return res.status(400).json({ msg: 'El precio mínimo debe ser menor o igual al precio máximo' });
+}
+const metalesPermitidos = ['oro', 'plata', 'bronce', 'plomo'];
+if (!metalesPermitidos.includes(metal)) {
+    return res.status(400).json({ msg: 'El metal proporcionado no es válido' });
+
+}
+
 if (precio_min){
     queryParams.push(precio_min);
     query += ' AND precio >= $' + queryParams.length;
-    }
-
+}
 if (precio_max){
     queryParams.push(precio_max);
     query += ' AND precio <= $' + queryParams.length;
