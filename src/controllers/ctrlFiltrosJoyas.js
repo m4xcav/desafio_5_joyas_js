ed7883d8fa249fc3bd4aef6e4f088efcbb1b72c4
HendyCastro <hendy0702@gmail.com>
2024-02-16 00:43:26 -0300

    modificacion en ctrlFiltrosJoyas

--- src/controllers/ctrlFiltrosJoyas.js
+++ src/controllers/ctrlFiltrosJoyas.js
@@ -5,6 +5,7 @@ const getJoyasPorFiltros = async ({ precio_min, precio_max, categoria, metal }) 
     if (precio_max) filtros.push(`precio <= ${precio_max}`);
     if (categoria) filtros.push(`categoria >= '${categoria}'`);
     if (metal) filtros.push(`metal = '${metal}'`);
+    
 
     let consulta = "SELECT * FROM inventario";
     if (filtros.length > 0) {
