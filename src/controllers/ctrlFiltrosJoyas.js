6827af44e906715bde1eb928848670576fcaa2bd
HendyCastro <hendy0702@gmail.com>
2024-02-16 00:23:16 -0300

    modificando

--- src/controllers/ctrlFiltrosJoyas.js
+++ src/controllers/ctrlFiltrosJoyas.js
@@ -1,8 +1,9 @@
+const db = require('../database/dbindex');
 const getJoyasPorFiltros = async ({ precio_min, precio_max, categoria, metal }) => {
     let filtros = [];
     if (precio_min) filtros.push(`precio >= ${precio_min}`);
     if (precio_max) filtros.push(`precio <= ${precio_max}`);
-    if (stock_min) filtros.push(`stock >= ${categoria}`);
+    if (stock_min) filtros.push(`categoria >= ${categoria}`);
     if (metal) filtros.push(`metal = '${metal}'`);
 
     let consulta = "SELECT * FROM joyas";
